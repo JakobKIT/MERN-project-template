c0413022abced85c968df73b25246a5864e3fddf
Jakob Klamser <mail@jakobklamser.net>
2020-04-12 16:13:52 +0200

    configured babel and webpack

---
+++ client/webpack/webpack.common.js
@@ -0,0 +1,67 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseURL = path.resolve(__dirname, '..');
+
+module.exports = {
+  entry: path.resolve(baseURL, 'src', 'index.js'),
+  output: {
+    path: path.resolve(baseURL, 'dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: '',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+              sourceMap: true,
+            },
+          },
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({}),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=10000&name=img/[name].[ext]',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${baseURL}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
---
+++ client/webpack/webpack.prod.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const merge = require('webpack-merge');
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+const common = require('./webpack.common');
+
+const options = {
+
+};
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [new RobotstxtPlugin(options)]
+});
