3060c0c0631fdebf017341e4e452a4e8a0c3ae7a
Jakob Klamser <mail@jakobklamser.net>
2020-04-13 19:57:29 +0200

    WIP: Login and register view started

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
+import LoginContainer from '../containers/auth/Login';
+import RegisterContainer from '../containers/auth/Register';
 import { login } from '../actions/authActions';
 
 export class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      isLogin: true,
+      userName: '',
+      email: '',
+      password: '',
+      password2: '',
+      msg: null,
+    };
   }
 
   static propTypes = {
+    isAuthenticated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
     onLogin: PropTypes.func.isRequired,
   };
+
+  componentDidUpdate(prevProps) {
+    const { error } = this.props;
+    if (error !== prevProps.error) {
+      if (error.id === 'LOGIN_FAIL') {
+        this.setState({ msg: error.msg });
+      } else {
+        this.setState({ msg: null });
+      }
+    }
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  switchForm(event) {
+    this.setState({ isLogin: !this.state.isLogin });
+  }
 
   login() {
     this.props.onLogin({
@@ -21,17 +51,34 @@ export class Home extends Component {
   }
 
   render() {
+    const { isLogin } = this.state;
     return (
-      <div>
-        Login
-        <button onClick={this.login.bind(this)}></button>
+      <div className="container-fluid">
+        { isLogin ? <LoginContainer 
+          onChange={this.onChange.bind(this)}
+          formSubmitted={this.login.bind(this)}
+          /> 
+          : <RegisterContainer
+          onChange={this.onChange.bind(this)}
+          formSubmitted={this.login.bind(this)}
+          />
+        }
+        <div className="container">
+          <a 
+            className="p-2 badge badge-pill badge-primary text-light float-right mt-2 mr-2 pointer"
+            onClick={this.switchForm.bind(this)}
+          >
+            { isLogin ? 'Need an account? Click here!' : 'Go to Login'}
+          </a>
+        </div>
       </div>
-    )
-  };
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    state
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error,
 });
 
 const mapDispatchToProps = (dispatch) => ({
