3b2698095a018d170d076d08c771efebbc125119
Jakob Klamser <mail@jakobklamser.net>
2020-04-13 22:09:19 +0200

    Email validation function

---
+++ client/src/utils/Validations.js
@@ -0,0 +1,4 @@
+// RegEx copied from https://emailregex.com/
+export const validateEmail = (email) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
